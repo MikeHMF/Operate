5c0f36f3ac8f8a35f1e99010e1c539130d08af11
meiFang <1507904849@qq.com>
2016-04-16 10:39:14 +0800

    hmm

--- jiMuOperate/Hall/JMNavigationController.m
+++ jiMuOperate/Hall/JMNavigationController.m
@@ -44,12 +44,7 @@
 {
     if (self.viewControllers.count > 0) { // 如果现在push的不是栈底控制器(最先push进来的那个控制器)
         viewController.hidesBottomBarWhenPushed = YES;
-        
-        // 设置导航栏按钮
-//        
-//        viewController.navigationItem.leftBarButtonItem = [UIBarButtonItem itemWithImageName:@"" highImageName:@"" target:self action:@selector(back)];
-//        viewController.navigationItem.rightBarButtonItem = [UIBarButtonItem itemWithImageName:@"navigationbar_more" highImageName:@"navigationbar_more_highlighted" target:self action:@selector(more)];
-        viewController.navigationController.navigationBar.backIndicatorImage = [UIImage imageNamed:@"navigationbar_more"];
+ 
     }
     [super pushViewController:viewController animated:animated];
 }
@@ -65,14 +60,5 @@
     [self popToRootViewControllerAnimated:YES];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
